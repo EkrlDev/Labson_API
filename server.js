559f98b21ba4b46edb3e4c550855828f345e3623
Ozar <ye.koral@gmail.com>
2020-05-16 09:27:38 +0300

    All methods templates created for CRUD oprations with pseudo functions

--- server.js
+++ server.js
@@ -6,6 +6,40 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+//Creating CRUD Methods
+//GET
+app.get('/api/v1/bootcamp', (req, res) => {
+  res.sendStatus(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+
+//GET ONE
+app.get('/api/v1/bootcamp/:id', (req, res) => {
+  res
+    .sendStatus(200)
+    .json({ success: true, msg: `Show bootcamp with id ${req.params.id}` });
+});
+
+//POST
+app.post('/api/v1/bootcamp/:id', (req, res) => {
+  res
+    .sendStatus(200)
+    .json({ success: true, msg: `Created bootcamp with id ${req.params.id}` });
+});
+
+//UPDATE
+app.put('/api/v1/bootcamp/:id', (req, res) => {
+  res
+    .sendStatus(200)
+    .json({ success: true, msg: `Updated bootcamp with id ${req.params.id}` });
+});
+
+//DELETE
+app.delete('/api/v1/bootcamp/:id', (req, res) => {
+  res
+    .sendStatus(200)
+    .json({ success: true, msg: `Deleted bootcamp with id ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
