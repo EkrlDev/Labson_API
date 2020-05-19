95859fe08fc85f621896ee371a7abc50559551e0
Ozar <ye.koral@gmail.com>
2020-05-19 04:24:20 +0300

    we added a daata file for sample data. We defined URI adress into config file. We also defined a db.js file into config folder for defining a module that connects us to database. We installed mongoose. we defined a models folder and into that folder we created Bootcamp.js file for defining model (a schema) of bootcamp record for our database. Under controllers folder at bootcamps.js we deleted pseudo functions for CRUD methods and defined real ones including  try-catch blocks. We add connectDB, colors requirements. We add express body parser. Finally we resolve the problem about package-json in start script

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,45 +1,84 @@
+const Bootcamp = require('../models/Bootcamp');
+
 // @desc         Get all bootcamps
 // @route        GET /api/v1/bootcamps
 // @access       Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+
+    res
+      .status(200)
+      .json({ success: true, count: bootcamps.length, data: bootcamps });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc         Get one bootcamps with id
 // @route        GET /api/v1/bootcamps/:id
 // @access       Public
-exports.getBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show bootcamp with id ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    // we make a validation first if there is a bootcamp with that ID
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+    //we returned abowe because if not it will try to send header secont time and crash the app
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc         Create new bootcamp
 // @route        POST /api/v1/bootcamps
 // @access       Private
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: `Created bootcamp with id ${req.params.id}`,
-  });
+exports.createBootcamp = async (req, res, next) => {
+  //every mondoose function is async
+  //here we handle rejection with trycatch but in the future, we will handle it with error handler middleware "async handler" so we will not need to define trycatch for each function.
+  try {
+    const newBootcamp = await Bootcamp.create(req.body);
+    //we create a new bootcamp at database with given data at req.body and return reponse
+    res.status(201).json({
+      success: true,
+      data: newBootcamp,
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc         Update a bootcamp
 // @route        PUT /api/v1/bootcamps/:id
 // @access       Private
-exports.updateBootcamp = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: `Updated bootcamp with id ${req.params.id}`,
-  });
+exports.updateBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    return res.status(400).json({ success: false });
+  }
 };
 
 // @desc         Delete a bootcamp
 // @route        DELETE /api/v1/bootcamps/:id
 // @access       Private
-exports.deleteBootcamp = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: `Deleted bootcamp with id ${req.params.id}`,
-  });
+exports.deleteBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: {} });
+  } catch (err) {
+    return res.status(400).json({ success: false });
+  }
 };
--- server.js
+++ server.js
@@ -2,14 +2,22 @@ const express = require('express');
 const dotenv = require('dotenv');
 //const logger = require('./middleware/logger'); we defined our custom middleware but we don't use it
 const morgan = require('morgan');
-
-//Initialise Routing files
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 //In order to use those config files we need to load env vars
 dotenv.config({ path: './config/config.env' });
 
+//Connect to database
+connectDB();
+
+//Initialise Routing files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+//Body parser. No longer do we have to use seperate body parser we handle it in express
+app.use(express.json());
 
 //Mount logger
 //app.use(logger);
@@ -24,7 +32,17 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+//Global handler for unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+
+  //Close server and exit process
+  server.close(() => process.exit(1));
+});
